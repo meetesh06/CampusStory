a7393dff2cdda788dc8a89e862f971012404b380
meeteshmehta4_mriirs <meeteshmehta4_mriirs@outlook.com>
2019-01-10 16:48:06 +0530

    before push noti

--- screens/helpers/Navigation.js
+++ screens/helpers/Navigation.js
@@ -1,5 +1,5 @@
 import { Navigation } from 'react-native-navigation'
-
+import Icon from 'react-native-vector-icons/AntDesign';
 export const goToInterestsSelector = () => Navigation.setRoot({
     root: {
         stack: {
@@ -36,84 +36,154 @@ export const goInitializing = () => Navigation.setRoot({
       }
 });
 
-export const goHome = () => Navigation.setRoot({
-  root: {
-    bottomTabs: {
-      id: 'BottomTabsId',
-      children: [
-        {
-          stack: {
-            id: "Home Stack",
-            options: {
-              topBar: {
-                visible: false
-              }
-            },
-            children: [
-              {
-                component: {
-                  name: 'Home Screen',
-                  options: {
-                    bottomTab: {
-                      fontSize: 10,
-                      selectedFontSize: 12,
-                      text: 'Home',
-                      icon: require('../../media/navigation/home-not.png'),
-                      selectedIcon: require('../../media/navigation/home.png')
-                    }
+export const goHome = async () => {
+  const homeIcon = await Icon.getImageSource('home', 30);
+  const areaChartIcon = await Icon.getImageSource('linechart', 30);
+  const profileIcon = await Icon.getImageSource('profile', 30);
+  return Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        id: 'BottomTabsId',
+        children: [
+          {
+            stack: {
+              id: "Home Stack",
+              options: {
+                topBar: {
+                  visible: false,
+                  drawBehind: true
+                },
+                bottomTab: {
+                  fontSize: 10,
+                  selectedFontSize: 12,
+                  text: 'Home',
+                  icon: homeIcon,
+                  iconColor: '#c0c0c0',
+                  textColor: '#c0c0c0',
+                  selectedIconColor: '#555'
+                }
+              },
+              children: [
+                {
+                  component: {
+                    name: 'Home Screen'
                   }
                 }
-              }
-            ]
-          }
-        },
-        // {
-        //   component: {
-        //     name: 'Home Screen',
-        //     options: {
-        //       bottomTab: {
-        //         fontSize: 10,
-        //         selectedFontSize: 12,
-        //         text: 'Home',
-        //         icon: require('../../media/navigation/home-not.png'),
-        //         selectedIcon: require('../../media/navigation/home.png')
-        //       }
-              
-        //     }
-        //   },
-        // },
-        {
-          component: {
-            name: 'Discover Screen',
-            options: {
-              bottomTab: {
-                text: 'Discover',
-                fontSize: 10,
-                selectedFontSize: 12,
-                icon: require('../../media/navigation/home-not.png'),
-                selectedIcon: require('../../media/navigation/home.png')
-              }
+              ]
             }
           },
-        },
-        {
-          component: {
-            name: 'Profile Screen',
-            options: {
-              bottomTab: {
-                text: 'Profile',
-                fontSize: 10,
-                selectedFontSize: 12,
-                icon: require('../../media/navigation/home-not.png'),
-                selectedIcon: require('../../media/navigation/home.png')
-              },
-              topBar: {
-                visible: false
+          {
+            component: {
+              name: 'Discover Screen',
+              options: {
+                bottomTab: {
+                  text: 'Discover',
+                  fontSize: 10,
+                  selectedFontSize: 12,
+                  icon: homeIcon,
+                  iconColor: '#c0c0c0',
+                  textColor: '#c0c0c0',
+                  selectedIconColor: '#555'
+                },
+                topBar: {
+                  visible: false,
+                  drawBehind: true
+                }
               }
-            }
+            },
           },
-        },
-      ],
+          {
+            component: {
+              name: 'Profile Screen',
+              options: {
+                bottomTab: {
+                  text: 'Profile',
+                  fontSize: 10,
+                  selectedFontSize: 12,
+                  icon: homeIcon,
+                  iconColor: '#c0c0c0',
+                  textColor: '#c0c0c0',
+                  selectedIconColor: '#555'
+                },
+                topBar: {
+                  visible: false,
+                  drawBehind: true
+                }
+              }
+            },
+          },
+        ],
+      }
     }
-  }
-});
+  });
+}
+
+    
+
+// export const goHome = () => Navigation.setRoot({
+//   root: {
+//     bottomTabs: {
+//       id: 'BottomTabsId',
+//       children: [
+//         {
+//           stack: {
+//             id: "Home Stack",
+//             options: {
+//               topBar: {
+//                 visible: false,
+//                 drawBehind: true
+//               }
+//             },
+//             children: [
+//               {
+//                 component: {
+//                   name: 'Home Screen',
+//                   options: {
+//                     bottomTab: {
+//                       fontSize: 10,
+//                       selectedFontSize: 12,
+//                       text: 'Home',
+//                       icon: require('../../media/navigation/home-not.png'),
+//                       selectedIcon: require('../../media/navigation/home.png')
+//                     }
+//                   }
+//                 }
+//               }
+//             ]
+//           }
+//         },
+//         {
+//           component: {
+//             name: 'Discover Screen',
+//             options: {
+//               bottomTab: {
+//                 text: 'Discover',
+//                 fontSize: 10,
+//                 selectedFontSize: 12,
+//                 icon: require('../../media/navigation/home-not.png'),
+//                 selectedIcon: require('../../media/navigation/home.png')
+//               }
+//             }
+//           },
+//         },
+//         {
+//           component: {
+//             name: 'Profile Screen',
+//             options: {
+//               bottomTab: {
+//                 text: 'Profile',
+//                 fontSize: 10,
+//                 selectedFontSize: 12,
+//                 icon: require('../../media/navigation/home-not.png'),
+//                 selectedIcon: require('../../media/navigation/home.png')
+//               },
+//               topBar: {
+//                 visible: false
+//               }
+//             }
+//           },
+//         },
+//       ],
+//     }
+//   }
+// });
